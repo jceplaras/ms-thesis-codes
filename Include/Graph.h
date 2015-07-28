b1ac706a03e62260767a62e8cc278862fc7eb12a
James Plaras <jamesplaras@gmail.com>
2015-07-28 12:40:03 +0800

    Overhaul of directory structure

---
+++ Include/Graph.h
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define FORN(i,N) for (int i = 0; i < N; i++)
+#define FORD(i,a,b) for (int i = a; i >= b; i--)
+
+#ifndef GRAPH_H
+#define GRAPH_H
+class Graph {
+    private:
+        std::vector<std::vector<int>> distanceMatrix;
+    public:
+        Graph(int numberOfNodes = 0);
+
+        //getters
+        int getNumberOfNodes() const ;
+        int getEdgeWeight(int i, int j) const ;
+        int getMaxCityDistance(int nodeNumber) const ;
+        //setters
+        void setNumberOfNodes(int numberOfNodes) ;
+        void setEdgeWeight(int i, int j, int weight) ;
+        //helper functions
+        //
+        static Graph createGraphFromStandardInput() {
+            Graph g;
+
+            int numberOfNodes;
+            std::cin >> numberOfNodes;
+
+            g.setNumberOfNodes(numberOfNodes);
+
+            int temp;
+            FORN(i,numberOfNodes)
+                FORN(j,numberOfNodes) {
+                    std::cin >> temp;
+                    g.setEdgeWeight(i,j,temp);
+                }
+
+            return g;
+        }
+
+};
+#endif
